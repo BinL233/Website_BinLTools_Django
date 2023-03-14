09cd8fc286c7238c6946473f165df6f02ddf1f4e
BinL233 <835741923@qq.com>
2023-03-14 16:51:36 -0400

    Add message board

--- BinLTools/BinLTools/views.py
+++ BinLTools/BinLTools/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Message
+from .forms import MessageForm
 
 def index(request):
     return render(request, "index.html")
@@ -8,3 +10,19 @@ def digitconvert(request):
 
 def reactiontest(request):
     return render(request, "reaction.html")
+
+
+def message_board(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = MessageForm()
+
+    messages = Message.objects.order_by('-created_at')
+    context = {
+        'form': form,
+        'messages': messages,
+    }
+    return render(request, 'index.html', context)
