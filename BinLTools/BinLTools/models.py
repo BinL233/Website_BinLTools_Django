8b221eaed91fccd0ba6ea4bf05d2d9b8dfe66a6c
BinL233 <835741923@qq.com>
2023-03-20 17:14:20 -0400

    Modified models.py

--- BinLTools/BinLTools/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class RandomSong(models.Model):
-    name = models.CharField(max_length=200)
-    artist = models.CharField(max_length=200)
-    url = models.CharField(max_length=200)
--- BinLTools/random_song/models.py
+++ BinLTools/random_song/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class RandomSong(models.Model):
+    name = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
